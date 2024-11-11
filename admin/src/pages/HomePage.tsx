12ceaf674dc84141e17e8e6956ab673814718f36
Marek Fodor <fodormarek5@gmail.com>
2024-11-11 16:01:37 +0100

    Refactor and remove some aspects of home page.

--- admin/src/pages/HomePage.tsx
+++ admin/src/pages/HomePage.tsx
@@ -1,70 +1,113 @@
-import { Box, Button, Flex, Grid, Main, Typography } from '@strapi/design-system';
+import {
+  Alert,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Link,
+  Loader,
+  Main,
+  Typography,
+} from '@strapi/design-system';
 import { useIntl } from 'react-intl';
 
-import { Plus, Upload } from '@strapi/icons';
-import { InformationSquare } from '@strapi/icons/symbols';
+import { ArrowLeft, Upload } from '@strapi/icons';
 import { PLUGIN_ID } from '../pluginId';
 import { useFetchClient } from '@strapi/admin/strapi-admin';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useLayoutEffect, useState } from 'react';
 
 const HomePage = () => {
+  const [ready, setReady] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const { formatMessage } = useIntl();
+
   const { get } = useFetchClient();
+  const navigate = useNavigate();
+
   const handler = () => {
-    get(`/${PLUGIN_ID}/`).then((res) => {
-      console.log(res);
-    });
   };
+
+  useLayoutEffect(() => {
+    get(`/${PLUGIN_ID}/`)
+      .then((res) => {
+        setReady(true);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setReady(true);
+      });
+  }, []);
 
   return (
     <Main>
-      <Box padding={{ initial: [10, 10] }}>
-        <div>{'<-'} back</div>
+      <Flex direction="column" padding={[5, 10]} justifyContent="start" alignItems="start" gap={6}>
+        <Flex>
+          <Link as={NavLink} startIcon={<ArrowLeft />} onClick={() => navigate(-1)}>
+            Back
+          </Link>
+        </Flex>
         <Flex alignItems="start" direction="column">
           <Typography variant="alpha">Deploy Dashboard</Typography>
           <Typography textColor="neutral400" variant="epsilon">
             Dashboard for deploying and monitoring status of deployments.
           </Typography>
         </Flex>
-        <div style={{ height: '40px' }}></div>
-        {/* spacer component*/}
-        <Grid.Root gap={4}>
-          <Box background="neutral0" width="fit-content" margin={50} padding={[6, 6]}>
-            <Flex gap={8}>
-              <Flex
-                alignItems="start"
-                direction="column"
-                padding={4}
-                borderRadius={'3px'}
-                borderColor={'neutral400'}
-                width={'fit-content'}
+
+        {!ready ? (
+          <Flex width="100%" height="100%" minHeight="400px" justifyContent="center">
+            <Loader big>{formatMessage({ id: 'cloudflare-pages.home.busy' })}</Loader>
+          </Flex>
+        ) : (
+          <>
+            {error && (
+              <Alert
+                title="Error"
+                closeLabel="Close"
+                onClose={() => setError(null)}
+                variant="danger"
               >
-                <Typography variant="delta" textColor="neutral600" padding={[0, 0, 4, 0]}>
-                  Cloudflare pages
-                </Typography>
-                <Flex gap={4} direction="column">
-                  <Flex gap={4}>
-                    <Typography width="140px">Production</Typography>
-                    <Button variant="default" startIcon={<Upload />} onClick={handler}>
-                      Deploy
-                    </Button>
-                    <Button variant="tertiary" startIcon={<InformationSquare />} onClick={handler}>
-                      Logs
-                    </Button>
-                  </Flex>
-                  <Flex gap={4}>
-                    <Typography width="140px">Preview</Typography>
-                    <Button variant="default" startIcon={<Upload />} onClick={handler}>
-                      Deploy
-                    </Button>
-                    <Button variant="tertiary" startIcon={<InformationSquare />} onClick={handler}>
-                      Logs
-                    </Button>
+                {error}
+              </Alert>
+            )}
+
+            <Grid.Root gap={4}>
+              <Box background="neutral0" width="fit-content" margin={50} padding={[6, 6]}>
+                <Flex gap={8}>
+                  <Flex
+                    alignItems="start"
+                    direction="column"
+                    padding={4}
+                    borderRadius={'3px'}
+                    borderColor={'neutral400'}
+                    width={'fit-content'}
+                  >
+                    <Typography variant="delta" textColor="neutral600" padding={[0, 0, 4, 0]}>
+                      Cloudflare pages
+                    </Typography>
+                    <Flex gap={4} direction="column">
+                      <Flex gap={4}>
+                        <Typography width="140px">Production</Typography>
+                        <Button variant="default" startIcon={<Upload />} onClick={handler}>
+                          Deploy
+                        </Button>
+                      </Flex>
+                      <Flex gap={4}>
+                        <Typography width="140px">Preview</Typography>
+                        <Button variant="default" startIcon={<Upload />} onClick={handler}>
+                          Deploy
+                        </Button>
+                      </Flex>
+                    </Flex>
                   </Flex>
                 </Flex>
-              </Flex>
-            </Flex>
-          </Box>
-        </Grid.Root>
-      </Box>
+              </Box>
+            </Grid.Root>
+          </>
+        )}
+      </Flex>
     </Main>
   );
 };
